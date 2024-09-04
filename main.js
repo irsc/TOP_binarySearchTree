2f6ccdd8949ed837e27f57994164adead495a7e0
irsc <rivero.scastilla@gmail.com>
2024-09-04 12:42:51 +0200

    main.js as driver script added

---
+++ main.js
@@ -0,0 +1,19 @@
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+let inputArray = Array.apply(null, Array(16)).map(()=> Math.floor(Math.random()*100));
+console.log(inputArray);
+let tree = new Tree(inputArray);
+console.log(tree);
+
+prettyPrint(tree.root);

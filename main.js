e036e004e0e3d696a8329a6e23990701a0633264
irsc <rivero.scastilla@gmail.com>
2024-09-12 11:26:42 +0200

    updated with calls to methods following assigment requirements

--- main.js
+++ main.js
@@ -40,3 +40,86 @@ prettyPrint(tree.root);
 tree.deleteItem(44);
 console.log("Delete");
 prettyPrint(tree.root);
+
+console.log("Level order:")
+tree.levelOrder((node)=>{
+    console.log(node.data);
+});
+
+console.log("Pre Order");
+tree.preOrder((node)=>{
+    console.log(node.data);
+});
+
+console.log("In Order");
+tree.inOrder((node)=>{
+    console.log(node.data);
+});
+
+console.log("Post Order");
+tree.postOrder((node)=>{
+    console.log(node.data);
+});
+
+console.log("Height");
+console.log(tree.height(tree.find(12)));
+console.log(tree.height(tree.find(15)));
+console.log(tree.height(tree.find(87)));
+console.log(tree.height(tree.find(37)));
+
+console.log("Depth");
+console.log(tree.depth(tree.find(12)));
+console.log(tree.depth(tree.find(67)));
+console.log(tree.depth(tree.find(87)));
+//console.log(tree.depth(4));
+
+prettyPrint(tree.root);
+console.log("is it balanced?");
+console.log(tree.isBalanced());
+
+console.log("let's remove some nodes");
+tree.deleteItem(15);
+tree.deleteItem(37);
+prettyPrint(tree.root);
+console.log("is it balanced?");
+console.log(tree.isBalanced());
+
+console.log("Let's get back to previous tree and add more nodes");
+tree.insert(15);
+tree.insert(37);
+tree.insert(120);
+tree.insert(110);
+tree.insert(132);
+tree.insert(131);
+tree.insert(133);
+prettyPrint(tree.root);
+console.log("is it balanced?");
+console.log(tree.isBalanced());
+
+console.log("let's rebalance it...");
+tree.rebalance();
+prettyPrint(tree.root);
+console.log("is it balanced?");
+console.log(tree.isBalanced());
+
+console.log("let's print the elements:")
+console.log("Level order:")
+tree.levelOrder((node)=>{
+    console.log(node.data);
+});
+
+console.log("Pre Order");
+tree.preOrder((node)=>{
+    console.log(node.data);
+});
+
+console.log("In Order");
+tree.inOrder((node)=>{
+    console.log(node.data);
+});
+
+console.log("Post Order");
+tree.postOrder((node)=>{
+    console.log(node.data);
+});
+
